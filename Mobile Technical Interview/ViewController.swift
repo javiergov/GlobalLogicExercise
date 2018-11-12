080ca74a48637b85490dbf916dbe48e78315e54c
Javier González Ovalle <jagonzalezo@falabella.cl>
2018-11-11 21:24:48 -0300

    Initial Commit

---
+++ Mobile Technical Interview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mobile Technical Interview
+//
+//  Created by Javier Cristóbal González Ovalle on 11/11/18.
+//  Copyright © 2018 GlobalLogic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
